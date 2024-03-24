210bd17b2dad1f352bafd8f585e472f2472ac703
Killerlord1988 <killerlord1988@gmail.com>
2024-03-24 21:36:31 +0400

    fix compare dates

--- main.js
+++ main.js
@@ -3,14 +3,21 @@ const cheerio = require('cheerio')
 
 const searchUrl =
   'https://www.astrgorod.ru/search/node/%D0%9F%D0%BB%D0%B0%D0%BD%D0%BE%D0%B2%D0%BE%D0%B5%20%D0%BE%D1%82%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D0%B5%20%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D1%8D%D0%BD%D0%B5%D1%80%D0%B3%D0%B8%D0%B8'
-const dateNow = new Date().toLocaleDateString()
+const dateNow = new Date()
 const sendMessage =
   ' http://pushmebot.ru/send?key=8dd3ec0dc7244fc6fa28b7ed91092dd3&message=Свет вырубят'
 const arr = []
 
+function settingDateFromLocal(strDate) {
+  const dateArr = strDate.split('.')
+  const date = new Date(dateArr[2], dateArr[1] - 1, dateArr[0])
+  return date
+}
+
 function matchWithNowDate(str, href) {
   const strDate = str.match(/(\d+).(\d+).(\d+)/)
-  if (strDate[0] >= '19.03.2024') {
+  const date = settingDateFromLocal(strDate[0])
+  if (date >= dateNow) {
     axios.get(href).then((html) => {
       const $ = cheerio.load(html.data)
       $('#node-content > table > tbody > tr > td > p').each((i, elem) => {
